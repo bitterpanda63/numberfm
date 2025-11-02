e65b7acf1e278215ca966d835c60e81365f4f485
BitterPanda <bitterpanda@proton.me>
2025-11-02 23:40:21 +0100

    Fix utils

--- utils.go
+++ utils.go
@@ -4,7 +4,7 @@ import "time"
 
 func bitDepthToIntegerRange(bitDepth int) float64 {
 	// 2^(bitDepth - 1) (the -1 is because it's signed lol)
-	return float64(1 << (bitDepth - 1))
+	return float64(int(1) << (bitDepth - 1))
 }
 
 func frequencyToPeriod(frequency float64) time.Duration {
